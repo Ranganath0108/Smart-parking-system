56e939365fe374258e8495c45dcc3ea22fb95c31
Ranganath <ranganathswamy.ys@gmail.com>
2021-08-05 22:44:48 +0530

    LCD code added

--- Parking/Parking.ino
+++ Parking/Parking.ino
@@ -1,7 +1,6 @@
+#include <LiquidCrystal_I2C.h>
 
-
-
-
+LiquidCrystal_I2C lcd(0x27,16,2);
 //intializing trigger pins
 #define trigA 4
 #define trigB 5 
@@ -24,7 +23,7 @@ int distB;
 int distC;
 
 void setup() {
-  
+
 //Output of arduino is the input to ultrasonic sensor
 pinMode(trigA, OUTPUT); 
 pinMode(trigB, OUTPUT); 
@@ -33,8 +32,11 @@ pinMode(trigC, OUTPUT);
 pinMode(echoA, INPUT);
 pinMode(echoB, INPUT);
 pinMode(echoC, INPUT);
-//pinMode(alaram,OUTPUT); 
-//digitalWrite(alaram,LOW);
+
+lcd.init();
+lcd.clear();
+lcd.backlight();
+  
 Serial.begin(9600); 
 }
 void loop() {
@@ -70,11 +72,23 @@ int valA=Available(distA);
 int valB=Available(distB);
 int valC=Available(distC);
 
-
-
+lcd.setCursor(0,0);
+lcd.print("SLOT");
+lcd.setCursor(0,6);
+lcd.print("A");
+lcd.setCursor(0,9);
+lcd.print("B");
+lcd.setCursor(0,12);
+lcd.print("C");
+lcd.setCursor(1,6);
+lcd.print(valA);
+lcd.setCursor(1,9);
+lcd.print(valB);
+lcd.setCursor(1,12);
+lcd.print(valC);
 
 }
-int Available(float distance){
+int Available(int distance){
   if(distance>=25)
   return 1;
   else 
